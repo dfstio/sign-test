f94db666afde34b9b526dfee9621c14fe00f8419
DFST <mk@dfst.io>
2024-05-21 00:36:56 +0300

    o1js

---
+++ vite.config.old.ts
@@ -0,0 +1,18 @@
+/*
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import swc from 'unplugin-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // Vite plugin
+    swc.vite(),
+    // Rollup plugin
+    //swc.rollup(),
+    react(),
+  ],
+});
+*/
